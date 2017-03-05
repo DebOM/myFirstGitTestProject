cbb285b4938252bf1383f3279eb11c0fe247a9b8
DebOM <debasishbd@hotmail.com>
2017-03-04 20:12:05 -0500

    javascript math methods

---
+++ Math_Methode.js
@@ -0,0 +1,9 @@
+  Math.PI  (a property) - Evaluates to mathematical PI
+• Math.abs(number)  - Returns the absolute of the  number
+• Math.ceil(number)  - Returns the smallest integer greater that or equal to the  number
+• Math.floor(number)  - Returns the largest integer less than or equal to the  number
+• Math.round(number)  - Returns the value of the  number  rounded to the nearest integer
+• Math.random()  - Return a (pseudo) random number between 0 and 1
+• Math.sqrt(number)  - Returns the square root of the  number
+• Math.pow(base,pow)  -
+• Math.floor(number)  - 
